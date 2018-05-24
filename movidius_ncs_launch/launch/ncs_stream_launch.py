166d9a0482b036fd360d52f0afaa3e6d7e5ae7f5
Xiaojun Huang <xiaojun.huang@intel.com>
2018-05-24 11:47:45 +0800

    fix the issues from "ament test"
    Signed-off-by: Xiaojun Huang <xiaojun.huang@intel.com>

--- movidius_ncs_launch/launch/ncs_stream_launch.py
+++ movidius_ncs_launch/launch/ncs_stream_launch.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,15 +15,16 @@
 from launch.exit_handler import default_exit_handler
 from ros2run.api import get_executable_path
 
+
 def launch(launch_descriptor, argv):
-  package = 'movidius_ncs_stream'
-  plugin = 'movidius_ncs_stream::NCSComposition'
+    package = 'movidius_ncs_stream'
+    plugin = 'movidius_ncs_stream::NCSComposition'
 
-  # run composition by api_composition_cli
-  launch_descriptor.add_process(
-    cmd=[get_executable_path(package_name='composition', executable_name='api_composition_cli'), package, plugin],
-    name='movidius_ncs_stream',
-    exit_handler=default_exit_handler,
-  )
-
-  return launch_descriptor
+    # run composition by api_composition_cli
+    launch_descriptor.add_process(
+        cmd=[get_executable_path(
+            package_name='composition',
+            executable_name='api_composition_cli'), package, plugin],
+        name='movidius_ncs_stream',
+        exit_handler=default_exit_handler,)
+    return launch_descriptor
